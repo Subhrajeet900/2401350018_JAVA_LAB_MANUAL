417f93f5d72cf7b0dabb3e1d2bbcf38821a66cbe
subhrajeet dash <subhrajeetdash900@gmail.com>
2025-11-23 14:25:06 +0530

    Rename class from lab_assignment1 to lab_assignment_1

--- lab_assignment_1.java
+++ lab_assignment_1.java
@@ -62,7 +62,7 @@ class Student {
     }
 }
 
-public class lab_assignment1 {
+public class lab_assignment_1 {
     public static void main(String[] args) {
       
         Scanner scanner = new Scanner(System.in);
